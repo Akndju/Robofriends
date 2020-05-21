8fa8b07e02a06a1bc97ab68be983fe534bb4fe2c
Akndju <davidd.akindoju@gmail.com>
2020-05-22 00:57:22 +0100

    Updates

---
+++ precache-manifest.2da05b4ce4a3bf7bbb0807787d427a8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686b0d627266b6545416dc8d9383dc07",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "537e8a2237e1150a84cd",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "9bd6e0582b9690ab3b8a",
+    "url": "/robofriends/static/css/main.2d2d64fa.chunk.css"
+  },
+  {
+    "revision": "537e8a2237e1150a84cd",
+    "url": "/robofriends/static/js/2.723846ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.723846ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd6e0582b9690ab3b8a",
+    "url": "/robofriends/static/js/main.9f07affa.chunk.js"
+  },
+  {
+    "revision": "3a6cd7bf0ee3b3b38bcf",
+    "url": "/robofriends/static/js/runtime-main.db3a75f1.js"
+  },
+  {
+    "revision": "c29724dbca733c1b552bdfee0db9b32d",
+    "url": "/robofriends/static/media/Doctor Glitch.c29724db.woff"
+  }
+]);
