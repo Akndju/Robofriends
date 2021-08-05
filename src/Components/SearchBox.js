b9f58cba2e62c31a9172ea442b30cc9248090c55
Akndju <davidd.akindoju@gmail.com>
2021-08-05 15:52:20 +0100

    Fixed redundnt code

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const SearchBox = ({searchfield, searchChange}) => {
-    return (
-        <div style = {{paddingBottom: '15px'}}>
-            <input className = 'pa2 ba b--black bg-white br3 tc bw0.8' 
-        type = 'search' placeholder = 'Search Robot'
-        onChange = {searchChange}/>
-        </div> 
-    )
-}
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div style={{ paddingBottom: "15px" }}>
+      <input
+        className="pa2 ba b--black bg-white br3 tc bw0.8"
+        type="search"
+        placeholder="Search Robot"
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
 
 export default SearchBox;
--- src/robots.js
+++ src/robots.js
@@ -1,51 +1,50 @@
-export const robots =  [
-    {
-        id: 1,
-        name: 'Chris Convenant',
-        username: 'QBC',
-        email: 'QBC@gmail.com'
-    },
-    {
-        id: 2,
-        name: 'Uweh George',
-        username: 'George',
-        email: 'GAMHNile@gmail.com'
-    },
-    {
-        id: 3,
-        name: 'Akndju',
-        username: 'Akndju',
-        email: 'Akndju@gmail.com'
-    },
-    {
-        id: 4,
-        name: 'Chikaodili',
-        username: 'Harrison',
-        email: 'Ofoje@gmail.com'
-    },
-    {
-        id: 5,
-        name: 'Sarah Towobee',
-        username: 'Sarah',
-        email: 'Teobola@gmail.com'
-    },
-    {
-        id: 6,
-        name: 'Fanny Anaso',
-        username: 'kene',
-        email: 'Fanaso@gmail.com'
-    },
-    {
-        id: 7,
-        name: 'Menim David',
-        username: 'Uwakmfon',
-        email: 'General@gmail.com'
-    },
-    {
-        id: 8,
-        name: 'Darlington Godwin',
-        username: 'Dannyto',
-        email: 'Dannyto@gmail.com'
-    },
-
-]
+export const robots = [
+  {
+    id: 1,
+    name: 'Chris Convenant',
+    username: 'QBC',
+    email: 'QBC@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Uweh George',
+    username: 'George',
+    email: 'GAMHNile@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Akndju',
+    username: 'Akndju',
+    email: 'Akndju@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Chikaodili',
+    username: 'Harrison',
+    email: 'Ofoje@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'Sarah Towobee',
+    username: 'Sarah',
+    email: 'Teobola@gmail.com',
+  },
+  {
+    id: 6,
+    name: 'Fanny Anaso',
+    username: 'kene',
+    email: 'Fanaso@gmail.com',
+  },
+  {
+    id: 7,
+    name: 'Menim David',
+    username: 'Uwakmfon',
+    email: 'General@gmail.com',
+  },
+  {
+    id: 8,
+    name: 'Darlington Godwin',
+    username: 'Dannyto',
+    email: 'Dannyto@gmail.com',
+  },
+];
