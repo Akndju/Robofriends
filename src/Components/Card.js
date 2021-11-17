15498a88adb752279ed6614cf785cb6f9a13f38b
Akndju <davidd.akindoju@gmail.com>
2021-11-17 16:27:23 +0100

    Optimized card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const card = (props) => {
+const card = ({ name, id, email }) => {
   return (
     <div className="bg-washed-blue dib br3 pa3 ma2 grow bw2 shadow-3 tc">
       <img
         width="150"
         height="150"
         alt="Robots"
-        src={`https://robohash.org/${props.id}?size=300x300`}
+        src={`https://robohash.org/${id}?size=300x300`}
       />
       <div>
-        <h1>{props.name}</h1>
-        <p>{props.email}</p>
+        <h1>{name}</h1>
+        <p>{email}</p>
       </div>
     </div>
   );
--- src/Components/Card.test.js
+++
@@ -1,7 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import Card from './Card';
-
-it('expect to render Card Component', () => {
-  expect(shallow(<Card />)).toMatchSnapshot();
-});
